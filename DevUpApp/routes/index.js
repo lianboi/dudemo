52aae3cad174fd89f0244cbe8ba5f6fd0744da3b
abs14690 <sharmaankur92@gmail.com>
2015-11-02 20:46:44 +0530

    jira auth test

--- DevUpApp/routes/index.js
+++ DevUpApp/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var sys = require('sys');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'DevUp - Index' });
 });
+
+router.post('/login', function(req, res, next){
+	console.log(req);
+	res.send("success!");
+});
+
+router.post('/testAuth', function(req, res, next){
+	var result = sys.exec('curl -D- -X GET -H "Authorization: Basic YWRtaW46dGVjaG5vY3ViZQ==" -H "Content-Type: application/json" "https://lbjiratest.atlassian.net/rest/auth/1/session"', function (error, stdout, stderr) {
+		console.log(error, stdout, stderr);
+	});
+	console.log(result);
+	//res.send(result);
+});
+
 
 module.exports = router;
